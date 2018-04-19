a62a49fb9e06d1ddf31452c1a8c961290de2bd67
Diego Sanabria <diegohsanabria@gmail.com>
2018-04-19 16:23:25 -0300

    libre accesso a imagenes

--- comandas2/config/web.php
+++ comandas2/config/web.php
@@ -131,6 +131,7 @@ $config = [
             //'site/*',
             //'gii/*',
             'admin/user/signup',
+            'yii2images/*',
             // The actions listed here will be allowed to everyone including guests.
             // So, 'admin/*' should not appear here in the production, of course.
             // But in the earlier stages of your development, you may probably want to
